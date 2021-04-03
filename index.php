70e0a4bbf2368e3cd1a365d97ed836b3598a3ea7
kouki-nisida <kouki19990309@gmail.com>
2021-04-03 14:49:57 +0900

    修正内容が分かるコミットメッセージ

--- index.php
+++ index.php
@@ -5,23 +5,4 @@ $Service="COACHTECH";
 echo $Company."は".$Service."を運営しています";
 
 
-for ($i = 1; $i <= 100; $i++) {
-   if ($i % 3 === 0 && $i % 5 === 0) {
-       echo "fizzbuzz";
-       echo "\n";
-    //    continue;
-   }
-   if ($i % 5 === 0) {
-       echo "buzz";
-       echo "\n";
-    //    continue;
-   }
-   if ($i % 3 === 0) {
-       echo "fizz";
-       echo "\n";
-    //    continue;
-   }
-   echo $i;
-   echo "\n";
-}
 ;
