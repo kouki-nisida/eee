f6c48d59b44c3e0f0b9da8fbe61494cfe6baa06d
kouki-nisida <kouki19990309@gmail.com>
2021-04-03 18:59:47 +0900

    test

--- index.php
+++ index.php
@@ -5,4 +5,23 @@ $Service="COACHTECH";
 echo $Company."は".$Service."を運営しています";
 
 
+for ($i = 1; $i <= 100; $i++) {
+   if ($i % 3 === 0 && $i % 5 === 0) {
+       echo "fizzbuzz";
+       echo "\n";
+    //    continue;
+   }
+   if ($i % 5 === 0) {
+       echo "buzz";
+       echo "\n";
+    //    continue;
+   }
+   if ($i % 3 === 0) {
+       echo "fizz";
+       echo "\n";
+    //    continue;
+   }
+   echo $i;
+   echo "\n";
+}
 ;
